acfa047c8cb8ec83b78eb4f7f86475e3f6c1aa48
Mikael John Andersson <mickepunktjohn@gmail.com>
2018-05-07 14:39:50 +0200

    Created project

---
+++ app/src/main/java/com/bimobject/themayproject/MainSearchActivity.java
@@ -0,0 +1,13 @@
+package com.bimobject.themayproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_search);
+    }
+}
