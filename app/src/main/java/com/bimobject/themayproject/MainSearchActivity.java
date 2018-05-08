7887e0263abe1a8ef16c021284b1f54fd6c26dda
Mikael John Andersson <mickepunktjohn@gmail.com>
2018-05-08 12:08:04 +0200

    search button

--- app/src/main/java/com/bimobject/themayproject/MainSearchActivity.java
+++ app/src/main/java/com/bimobject/themayproject/MainSearchActivity.java
@@ -2,6 +2,7 @@ package com.bimobject.themayproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainSearchActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_search);
+
+        EditText searchBox = findViewById(R.id.searchBox);
+        searchBox.getText();
+
     }
 }
