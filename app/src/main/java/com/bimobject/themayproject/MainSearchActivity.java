c31304355cff85af1ae200eb35b2fa20f322b946
Mikael John Andersson <mickepunktjohn@gmail.com>
2018-05-08 12:20:09 +0200

    Adding comments

--- app/src/main/java/com/bimobject/themayproject/MainSearchActivity.java
+++ app/src/main/java/com/bimobject/themayproject/MainSearchActivity.java
@@ -14,14 +14,19 @@ public class MainSearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_search);
 
+        //Getting button from layout
         Button searchButton = findViewById(R.id.searchButton);
 
+        //Setting onClickListener
         searchButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
                 EditText searchBox = findViewById(R.id.searchBox);
                 TextView searchResult = findViewById(R.id.searchResult);
+
+                //Getting string value from searchbox
                 String search = searchBox.getText().toString();
+                //Setting text of searchResult to response from request
                 searchResult.setText(RequestService.getRequest(search, "/products"));
             }
         });
