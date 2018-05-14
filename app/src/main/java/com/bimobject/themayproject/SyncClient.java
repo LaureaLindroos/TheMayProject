f39a2710b7d294eb137325208a65cc6244377cea
LauresLindroos <laurea.lindroos@hotmail.com>
2018-05-14 15:26:31 +0200

    New try - made new class for syncHttpClient

--- app/src/main/java/com/bimobject/themayproject/SyncClient.java
+++ app/src/main/java/com/bimobject/themayproject/SyncClient.java
@@ -1,8 +1,23 @@
 package com.bimobject.themayproject;
 
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.ResponseHandlerInterface;
 import com.loopj.android.http.SyncHttpClient;
 
-public class AsyncClient {
-    private static SyncHttpClient client = new SyncHttpClient();
-    private static final String BASE_URL = "https://api.bimobject.com/search/v1/";
+public class SyncClient {
+    static final SyncHttpClient client = new SyncHttpClient();
+    static final String BASE_URL = "https://api.bimobject.com/search/v1/";
+
+    public static void get(String url, RequestParams params, SyncHttpClient responseHandler) {
+        client.get(getAbsoluteUrl(url), params, (ResponseHandlerInterface) responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, SyncHttpClient responseHandler) {
+        client.post(getAbsoluteUrl(url), params, (ResponseHandlerInterface) responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
 }
+
