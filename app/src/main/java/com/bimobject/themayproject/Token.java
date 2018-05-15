c1b27111cd0b07701bdbb77750482188753d0f93
Mikael John Andersson <mickepunktjohn@gmail.com>
2018-05-15 15:50:32 +0200

    Code cleanup

--- app/src/main/java/com/bimobject/themayproject/Token.java
+++ app/src/main/java/com/bimobject/themayproject/Token.java
@@ -1,8 +1,5 @@
 package com.bimobject.themayproject;
 
-import android.os.Looper;
-
-import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.SyncHttpClient;
@@ -15,23 +12,30 @@ import java.util.TimerTask;
 
 import cz.msebera.android.httpclient.Header;
 
-public class Token {
+public final class Token {
 
     private static String accessToken;
-    final int expieryTime = 1350 * 1000;
+    private final int expieryTime = 1350 * 1000;
     private static Token instance;
-    Timer t;
+    private static final RequestParams params = new RequestParams();
+    private final Timer t;
 
-    private Token(
+    static {
+        params.put("grant_type", "client_credentials");
+        params.put("scope", "search_api");
+        params.put("client_id", "KaHKuGnJJduQ9Ek1ekXRw6PdLKTkdic7");
+        params.put("client_secret", "3yUKdB0agDKJlw7ltRwQ4eRTeZC2Fw22KmMNcRYfvgzQ0WxekewFfUJxhkknM7Lb");
+    }
 
-    ) {
+    private Token() {
         t = new Timer();
         t.scheduleAtFixedRate(new CollectAccessToken(), 0, expieryTime);
     }
 
     public static Token getInstance(){
         if (instance==null){
-            Token instance=new Token();
+
+            instance=new Token();
         }
         return instance;
     }
@@ -46,22 +50,11 @@ public class Token {
         return accessToken;
     }
 
-    //Method that collects AccessToken and sets the value.
     class CollectAccessToken extends TimerTask {
         SyncHttpClient client = new SyncHttpClient();
 
         @Override
         public void run() {
-            //skapar våra request parameters
-            RequestParams params = new RequestParams();
-            params.put("grant_type", "client_credentials");
-            params.put("scope", "search_api");
-            params.put("client_id", "KaHKuGnJJduQ9Ek1ekXRw6PdLKTkdic7");
-            params.put("client_secret", "3yUKdB0agDKJlw7ltRwQ4eRTeZC2Fw22KmMNcRYfvgzQ0WxekewFfUJxhkknM7Lb");
-
-
-            //Vi hämtar nya access tokens genom att göra post-requests till API:t innehållandes våra client credentials
-            //och nyckel
 
             client.post("https://accounts.bimobject.com/identity/connect/token", params, new JsonHttpResponseHandler(
             ) {
