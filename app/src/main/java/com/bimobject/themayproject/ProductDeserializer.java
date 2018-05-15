46a448e598948eb06a1d5c607177089e168f525c
Marie-Charlotte Persson <mariecharlotte.persson@gmail.com>
2018-05-15 10:10:43 +0200

    Object classes created with incomplete deserializer

---
+++ app/src/main/java/com/bimobject/themayproject/ProductDeserializer.java
@@ -0,0 +1,30 @@
+package com.bimobject.themayproject;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+
+public class ProductDeserializer implements JsonDeserializer<Product> {
+
+    @Override
+    public Product deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject jsonObject = json.getAsJsonObject();
+
+        return new Product(
+                jsonObject.get("id").getAsString(),
+                jsonObject.get("name").getAsString(),
+                jsonObject.get("status").getAsString(),
+                jsonObject.get("permalink").getAsString(),
+                jsonObject.get("imageUrl").getAsString()
+        );
+    }
+
+}
+
+
