378c1774b9dfb6898f255ab2aaa0d4404a2eeb9a
unknown <dvalemark@gmail.com>
2018-05-17 11:01:07 +0200

    added relative Layout for searchButton and making it clickable

--- app/src/main/java/com/bimobject/themayproject/TopBarFragment.java
+++ app/src/main/java/com/bimobject/themayproject/TopBarFragment.java
@@ -1,5 +1,4 @@
 package com.bimobject.themayproject;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,8 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class TopBarFragment extends Fragment {
     // The onCreateView method is called when Fragment should create its View object hierarchy,
@@ -29,7 +28,7 @@ public class TopBarFragment extends Fragment {
     // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-      ImageView searchButtonTopBar = view.findViewById(R.id.searchButtonTopBarImage);
+      RelativeLayout searchButtonTopBar = view.findViewById(R.id.searchButtonTopBar);
         searchButtonTopBar.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
