5677e45b48e5e810e5f4c26450d4de579e0296cf
unknown <dvalemark@gmail.com>
2018-05-21 10:29:54 +0200

    Adding enter as a search function for MainSearchActivity

--- app/src/main/java/com/bimobject/themayproject/ui/mainsearchactivity/MainSearchActivity.java
+++ app/src/main/java/com/bimobject/themayproject/ui/mainsearchactivity/MainSearchActivity.java
@@ -3,9 +3,12 @@ package com.bimobject.themayproject.ui.mainsearchactivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.bimobject.themayproject.R;
 import com.bimobject.themayproject.helpers.TokenGenerator;
@@ -32,6 +35,41 @@ public class MainSearchActivity extends AppCompatActivity {
                 intent.putExtra("search", search);
                 startActivity(intent);
 
+            }
+        });
+
+        searchButton.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                switch (actionId) {
+
+                    case EditorInfo.IME_ACTION_DONE:
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
+        searchButton.setOnKeyListener(new View.OnKeyListener() {
+
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    onRightButtonClicked();
+                    return true;
+                }
+                return false;
+            }
+
+            private void onRightButtonClicked() {
+                EditText searchBox = findViewById(R.id.searchBox);
+                String search = searchBox.getText().toString();
+
+
+                Intent intent = new Intent(MainSearchActivity.this, SearchResultActivity.class);
+                intent.putExtra("search", search);
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/bimobject/themayproject/ui/searchresultactivity/TopBarFragment.java
+++ app/src/main/java/com/bimobject/themayproject/ui/searchresultactivity/TopBarFragment.java
@@ -66,7 +66,11 @@ public class TopBarFragment extends Fragment {
 
 
         });
+
+
+
     }
+
 
 
 }
