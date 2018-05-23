be630207d55bd4ee2cc175e7744eeb0002673f77
unknown <dvalemark@gmail.com>
2018-05-23 14:09:21 +0200

    Implementing a ExpandableList and getting the code for the image url:s

--- app/src/main/java/com/bimobject/themayproject/ui/productinfoactivity/ProductInfoActivity.java
+++ app/src/main/java/com/bimobject/themayproject/ui/productinfoactivity/ProductInfoActivity.java
@@ -4,16 +4,25 @@ import android.os.AsyncTask;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 
 import com.bimobject.themayproject.R;
+import com.bimobject.themayproject.adapters.ExpandableListAdapter;
 import com.bimobject.themayproject.adapters.ViewPagerAdapter;
 import com.bimobject.themayproject.dto.ProductDetails;
 import com.bimobject.themayproject.helpers.RequestService;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class ProductInfoActivity extends AppCompatActivity {
 
     ViewPager viewPager;
+    ArrayList<String> listDataHeader;
+    HashMap <String,List<String>> listDataChild;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +33,21 @@ public class ProductInfoActivity extends AppCompatActivity {
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
         viewPager.setAdapter(viewPagerAdapter);
 
+        // get the listview
+        ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.activity_product_info_lvExp);
+
+        // preparing list data
+        prepareListData();
+
+        ExpandableListAdapter listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expandableListView.setAdapter(listAdapter);
+
+
         String productId = getIntent().getStringExtra("productId");
         new getProductDetailsTask().execute(productId);
+
 
     }
     private class getProductDetailsTask extends AsyncTask<String, String, ProductDetails>{
@@ -40,5 +62,18 @@ public class ProductInfoActivity extends AppCompatActivity {
         protected ProductDetails doInBackground(String... strings) {
             return RequestService.getProductDetails(strings[0]);
         }
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        listDataHeader.add("Description");
+
+        ArrayList <String> description = new ArrayList<String>();
+        description.add("Here is an awesome Description");
+
+        listDataChild.put(listDataHeader.get(0), description);
+
     }
 }
