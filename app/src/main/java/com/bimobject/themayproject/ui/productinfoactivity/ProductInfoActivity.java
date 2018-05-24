30d0f2649960439fc1d7e6067e14b41f6ae06227
Marie-Charlotte Persson <mariecharlotte.persson@gmail.com>
2018-05-24 13:14:55 +0200

    Added basic functionality for image slider dots, keeping track of which image you're at.

--- app/src/main/java/com/bimobject/themayproject/ui/productinfoactivity/ProductInfoActivity.java
+++ app/src/main/java/com/bimobject/themayproject/ui/productinfoactivity/ProductInfoActivity.java
@@ -1,9 +1,12 @@
 package com.bimobject.themayproject.ui.productinfoactivity;
 
 import android.os.AsyncTask;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bimobject.themayproject.R;
@@ -14,6 +17,10 @@ import com.bimobject.themayproject.helpers.RequestService;
 public class ProductInfoActivity extends AppCompatActivity {
 
     ViewPager viewPager;
+    LinearLayout sliderDotspanel;
+    int dotscount;
+    ImageView[] dots;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +40,51 @@ public class ProductInfoActivity extends AppCompatActivity {
             product_title.setText(productDetails.getName());
 
             viewPager = findViewById(R.id.activity_product_info_view_pager);
+            sliderDotspanel = (LinearLayout) findViewById(R.id.activity_product_info_ll_slider_dots);
+
             ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getApplicationContext(), productDetails.getImageUrls());
             viewPager.setAdapter(viewPagerAdapter);
+
+            dotscount = viewPagerAdapter.getCount();
+            dots = new ImageView[dotscount];
+
+            for(int i = 0; i < dotscount; i++){
+
+                dots[i] = new ImageView(getApplicationContext());
+                dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.tab_indicator_default));
+
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+                params.setMargins(8, 0, 8, 0);
+
+                sliderDotspanel.addView(dots[i], params);
+
+                dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.tab_indicator_selected));
+
+                viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                    @Override
+                    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                    }
+
+                    @Override
+                    public void onPageSelected(int position) {
+
+                        for(int i = 0; i< dotscount; i++){
+                            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.tab_indicator_default));
+                        }
+
+                        dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.tab_indicator_selected));
+
+                    }
+
+                    @Override
+                    public void onPageScrollStateChanged(int state) {
+
+                    }
+                });
+
+            }
 
         }
 
