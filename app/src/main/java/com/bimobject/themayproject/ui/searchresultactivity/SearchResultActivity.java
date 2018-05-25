555eb0b5f117e762dc0a714935c33c809ff1fb2a
Martina J:son Gren <martinajsongren@hotmail.se>
2018-05-25 14:00:05 +0200

    Filter drawer (#43)
    * added initial functionality for filter drawer
    
    * Added basic filter functionality

--- app/src/main/java/com/bimobject/themayproject/ui/searchresultactivity/SearchResultActivity.java
+++ app/src/main/java/com/bimobject/themayproject/ui/searchresultactivity/SearchResultActivity.java
@@ -3,10 +3,21 @@ package com.bimobject.themayproject.ui.searchresultactivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
+
 
 import com.bimobject.themayproject.adapters.RecycleViewAdapter;
 import com.bimobject.themayproject.R;
@@ -14,11 +25,14 @@ import com.bimobject.themayproject.constants.STRINGS;
 import com.bimobject.themayproject.helpers.Request;
 import com.bimobject.themayproject.ui.productinfoactivity.ProductInfoActivity;
 
-
-public class SearchResultActivity extends AppCompatActivity {
+public class SearchResultActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private RecycleViewAdapter adapter;
     private static String search;
+    private RecyclerView recyclerView;
+    private DrawerLayout drawer;
+    private Request request;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,24 +43,26 @@ public class SearchResultActivity extends AppCompatActivity {
             search = getIntent().getStringExtra("search");
         }
 
-        Request request = new Request();
+        //DRAWER START
+        drawer = findViewById(R.id.drawer_layout);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //DRAWER END
+
+        request = new Request();
         request.addSearch(search);
-
-        /*
-        Button buttonCategory = findViewById(R.id.activity_serch_result_btn_filter);
-        buttonCategory.setOnClickListener(view -> {
-
-            request.addCategory("137");
-            adapter.makeNewRequest(request);
-
-        });
-
-        Button btnClear = findViewById(R.id.activity_search_result_btn_filter_clear);
-        btnClear.setOnClickListener(view -> {
-            request.clearParams();
-            adapter.makeNewRequest(request);
-        });
-        */
 
 
         adapter = new RecycleViewAdapter();
@@ -72,5 +88,55 @@ public class SearchResultActivity extends AppCompatActivity {
 
     }
 
+    //DRAWER CONTINUE
 
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.END)) {
+            drawer.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //noinspection SimplifiableIfStatement
+        switch (item.getItemId()) {
+            case R.id.action_drawer:
+                drawer.openDrawer(GravityCompat.END);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.drawer_filter_search_action) {
+            request.addCategory("137");
+            adapter.makeNewRequest(request);
+        } else if (id == R.id.drawer_filter_search_clear) {
+            request.clearParams();
+            adapter.makeNewRequest(request);
+        }
+
+        //TODO Functionality for buttons
+
+        drawer.closeDrawer(GravityCompat.END);
+        return true;
+    }
+    //DRAWER FINISHED
 }
